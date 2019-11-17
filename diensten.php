7a34d1ba9788f3b83a219d29e547a8b9f3a5158b
Jochem de Wit <43673113+26278@users.noreply.github.com>
2019-11-17 22:50:24 +0100

    Initial Commit

---
+++ diensten.php
@@ -0,0 +1,238 @@
+<body>
+	<header>
+		<div class="header__overlay">
+			<div class="header__top">
+				<img src="public/img/logo_white.svg" alt="YoungVisual Logo">
+                <ul>
+                    <li onclick="location.href = '/'">Home</li>
+                    <li onclick="location.href = 'diensten'" class="active">Diensten</li>
+                    <li onclick="location.href = 'over-ons'">Over ons</li>
+                    <li onclick="location.href = 'portfolio'">Portfolio</li>
+                    <li onclick="location.href = 'contact'">Contact</li>
+                </ul>
+				<!-- <button>Offerte Aanvragen</button> -->
+			</div>
+			<div class="header__title">
+				<h1>Diensten die wij uitleveren</h1>
+				<h2>We zijn gespecialiseerd in WordPress Development & Online Marketing</h2>
+			</div>
+            <img class="header__overlay--corner" src="public/img/corner.png" alt="corner">
+		</div>
+	</header>
+	<main>
+	    <div class="project-uitleg">
+	        <h2 class="project-uitleg--title">Alles in één pakket</h2>
+	        <p class="project-uitleg--bericht">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+	            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+	            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+	            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+	            anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+	            incididunt ut labore et dolore magna aliqua. Ut
+	        </p>
+	        <img class="project-uitleg--img" src="public/img/screen_sideways.png" alt="screen/sideways">
+
+	        <div class="project-uitleg__property">
+	            <div class="project-uitleg__property__container">
+	                <img class="project-uitleg--icon" src="public/img/tech.png" alt="">
+	                <h2 class="project-uitleg__property--title">Website</h2>
+	            </div>
+	            <p class="project-uitleg__property__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+	                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+	                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+	                eu fugiat nulla pariatur.
+	            </p>
+	        </div>
+	        <div class="project-uitleg__property">
+	            <div class="project-uitleg__property__container">
+	                <img class="project-uitleg--icon" src="public/img/tools.png" alt="">
+	                <h2 class="project-uitleg__property--title">Huisstyle</h2>
+	            </div>
+	                <p class="project-uitleg__property__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+	                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+	                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+	                    eu fugiat nulla pariatur.
+	                </p>
+	        </div>
+
+	        <div class="project-uitleg__property">
+	            <div class="project-uitleg__property__container">
+	                <img class="project-uitleg--icon" src="public/img/shop.png" alt="">
+	                <h2 class="project-uitleg__property--title">Online Marketing</h2>
+	            </div>
+	                <p class="project-uitleg__property__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+	                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+	                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+	                    eu fugiat nulla pariatur.
+	                </p>
+	        </div>
+	        <a class="project-uitleg__offerte" href="#">
+	            <button class="project-uitleg__offerte--button">Offerte Aanvragen</button>
+	        </a>
+	    </div>
+	    <div class="keuze-pakket">
+	        <h2 class="keuze-pakket--title">Keuze Pakket</h2>
+	        <p class="keuze-pakket--paragraph">
+	            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+	            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+	            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+	            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+	            anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+	            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+	            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+	            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+	            qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+	            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+	            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.
+	        </p>
+	    </div>
+	    <div class="pakketten">
+	        <div class="pakketten__website">
+	            <div class="pakketten__container">
+	                <img class="pakketten--icon" src="public/img/tech.png" alt="">
+	                <h2 class="pakketten--title">Website</h2>
+	            </div>
+	            <img class="pakketten--website-img" src="public/img/screen_front.png" alt="screen_front">
+	            <p class="pakketten--text">
+	                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+	                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+	            </p>
+	            <div class="checklists">
+	                <div class="pakketten__checklist">
+	                    <img class="pakketten__checklist--check" src="public/img/check.svg" alt="check">
+	                    <p class="pakketten__checklist--text">Éen domeinnaam</p>
+	                </div>
+	                <div class="pakketten__checklist">
+	                    <img class="pakketten__checklist--check" src="public/img/check.svg" alt="check">
+	                    <p class="pakketten__checklist--text">Website volledig verzorgd</p>
+	                </div>
+	                <div class="pakketten__checklist">
+	                    <img class="pakketten__checklist--check" src="public/img/check.svg" alt="check">
+	                    <p class="pakketten__checklist--text">Uitleg wordpress</p>
+	                </div>
+	            </div>
+	            <a class="pakketten__offerte" href="#">
+	                <button class="pakketten__offerte--button">Offerte Aanvragen</button>
+	            </a>
+	        </div>
+
+
+	        <div class="pakketten__huisstyle">
+	            <div class="pakketten__container">
+	                <img class="pakketten--icon" src="public/img/tools.png" alt="huisstyle">
+	                <h2 class="pakketten--title">Huisstyle</h2>
+	            </div>
+	            <img class="pakketten--huisstijl-img" src="public/img/website.png" alt="website">
+	            <p class="pakketten--text">
+	                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+	                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+	            </p>
+	            <div class="checklists">
+	                <div class="pakketten__checklist">
+	                    <img class="pakketten__checklist--check" src="public/img/check.svg" alt="check">
+	                    <p class="pakketten__checklist--text">Éen domeinnaam</p>
+	                </div>
+	                <div class="pakketten__checklist">
+	                    <img class="pakketten__checklist--check" src="public/img/check.svg" alt="check">
+	                    <p class="pakketten__checklist--text">Website volledig verzorgd</p>
+	                </div>
+	                <div class="pakketten__checklist">
+	                    <img class="pakketten__checklist--check" src="public/img/check.svg" alt="check">
+	                    <p class="pakketten__checklist--text">Uitleg wordpress</p>
+	                </div>
+	            </div>
+	            <a class="pakketten__offerte" href="#">
+	                <button class="pakketten__offerte--button">Offerte Aanvragen</button>
+	            </a>
+	        </div>
+
+	        <div class="pakketten__marketing">
+	            <div class="pakketten__container">
+	                <img class="pakketten--icon" src="public/img/shop.png" alt="marketing">
+	                <h2 class="pakketten--title">Online Marketing</h2>
+	            </div>
+	            <img class="pakketten__img-container--img" src="public/img/shirt.png" alt="shirt">
+	            <p class="pakketten--text">
+	                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+	                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+	            </p>
+	            <div class="checklists">
+	                <div class="pakketten__checklist">
+	                    <img class="pakketten__checklist--check" src="public/img/check.svg" alt="check">
+	                    <p class="pakketten__checklist--text">Éen domeinnaam</p>
+	                </div>
+	                <div class="pakketten__checklist">
+	                    <img class="pakketten__checklist--check" src="public/img/check.svg" alt="check">
+	                    <p class="pakketten__checklist--text">Website volledig verzorgd</p>
+	                </div>
+	                <div class="pakketten__checklist">
+	                    <img class="pakketten__checklist--check" src="public/img/check.svg" alt="check">
+	                    <p class="pakketten__checklist--text">Uitleg wordpress</p>
+	                </div>
+	            </div>
+	            <a class="pakketten__offerte" href="#">
+	                <button class="pakketten__offerte--button">Offerte Aanvragen</button>
+	            </a>
+	        </div>
+	    </div>
+	</main>
+
+    <footer class="footer">
+    <div class="layer">
+        <div class="footer__grid">
+            <div class="footer__grid__left">
+                <div class="footer__grid__left__marketing">
+                    <h2 class="footer--h2">Online Marketing</h2>
+                    <a class="footer--a" href="#">SEA: Zoekmachine adverteren</a><br>
+                    <a class="footer--a" href="#">SEO: zoekmachine optimalisatie</a><br>
+                    <a class="footer--a" href="#">Google Shopping adverteren</a>
+                </div>
+                <div class="footer__grid__left__wordpress">
+                    <h2 class="footer--h2">Wordpress Development</h2>
+                    <a class="footer--a" href="#">WordPress Development</a><br>
+                    <a class="footer--a" href="#">WordPress website laten maken</a><br>
+                    <a class="footer--a" href="#">Webshop laten maken</a><br>
+                    <a class="footer--a" href="#">Webdesign</a>
+                </div>
+            </div>
+            <div class="footer__grid__middle">
+                <div class="footer__grid__middle__socialmedia">
+                    <h2 class="footer--h2">Social Media</h2>
+                    <p class="footer--p">Het bouwen van WordPress-websites en het goed vindbaar maken van websites door middel van SEA en
+                            SEO is onze passie. Lees hier onze visie op Online Marketing en WordPress Development.
+                    </p>
+                </div>
+                <div class="footer__grid__middle__media-icons">
+
+                </div>
+            </div>
+            <div class="footer__grid__right">
+                <div class="footer__grid__right__youngvisual">
+                    <h2 class="footer--h2">Young Visual</h2>
+                    <p class="footer--p">Contactweg 36<br>
+                            1014 AN Amsterdam<br>
+                            020 -12 34 567<br>
+                            info@youngvisual.nl
+                    </p>
+                </div>
+                <div class="footer__grid__right__registers">
+                    <p class="footer--p">KVK nummer: 12345678 <br>
+                        BTW nummer: NL123456789A12 <br>
+                        IBAN: NL12 INGB 0123 4567 89
+                    </p>
+                    </div>
+            </div>
+        </div>
+
+        <hr class="footer--hr">
+
+        <div class="footer__bottomtext">
+            <div class="footer__bottomtext__top">
+                <a class="footer--a footer__bottomtext__top__inline" href="#">Algemene voorwaarden</a>
+                <p class="footer--a footer__bottomtext__top__inline"> | </p>
+                <a class="footer--a footer__bottomtext__top__inline" href="#">Privacy Policy</a>
+            </div>
+            <p class="footer__bottomtext--copyright">©2019 Young Visual</p>
+        </div>
+    </div>
+    </footer>
+</body>
---
+++ home.php
@@ -0,0 +1,171 @@
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+<body>
+	<header>
+		<div class="header__overlay">
+			<div class="header__top">
+				<img src="public/img/logo_white.svg" alt="YoungVisual Logo">
+				<ul>
+                    <li onclick="location.href = '/'" class="active">Home</li>
+                    <li onclick="location.href = 'diensten'">Diensten</li>
+                    <li onclick="location.href = 'over-ons'">Over ons</li>
+                    <li onclick="location.href = 'portfolio'">Portfolio</li>
+                    <li onclick="location.href = 'contact'">Contact</li>
+				</ul>
+				<!-- <button>Offerte Aanvragen</button> -->
+			</div>
+			<div class="header__title">
+				<h1>Making brands visible.</h1>
+				<h2>We zijn gespecialiseerd in WordPress Development & Online Marketing</h2>
+			</div>
+            <img class="header__overlay--corner" src="/public/img/corner.png" alt="corner">
+		</div>
+	</header>
+
+	<main class="main">
+        <div class="tips">
+            <div class="tip">
+               <img class="tip--icon" src="public/img/tech.png" alt="tip img">
+               <h2 class="tip--title">Website</h2>
+               <p class="tip--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="tip">
+               <img class="tip--icon" src="public/img/tools.png" alt="tip img">
+               <h2 class="tip--title">Huisstyle</h2>
+               <p class="tip--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="tip">
+               <img class="tip--icon" src="public/img/shop.png" alt="tip img">
+               <h2 class="tip--title">Online Marketing</h2>
+               <p class="tip--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+        <img class="corner" src="public/img/corner-left.png" alt="corner">
+        <div class="visie-overlay">
+            <div class="visie-top">
+                <h2 class="visie--title">Onze visie</h2>
+                <h3 class="visie--subtitle">Bij Young Visual zetten we ons in voor de groei van uw onderneming.</h3>
+                <p class="visie--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+        <div class="team">
+            <h2 class="team--title">Ons Team</h2>
+            <p class="team--text">Dit zijn de mannen van Young Visual. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="team-members">
+                <div class="team-members__member">
+                    <img class="team-members__member--img" src="public/img/img_avatar.png" alt="person image">
+                    <a class="team-members__member--link" href="#">
+                        <h3 class="team-members__member--name">Midas
+                            <img class="team-members__member--linkedin" src="/public/img/linked_in_icon.png" alt="linkedin">
+                        </h3>
+                    </a>
+                    <p class="team-members__member--function">MEDIA VORMGEVER</p>
+                </div>
+                <div class="team-members__member">
+                    <img class="team-members__member--img" src="public/img/img_avatar.png" alt="person image">
+                    <a class="team-members__member--link" href="#">
+                        <h3 class="team-members__member--name">Ferdi
+                            <img class="team-members__member--linkedin" src="/public/img/linked_in_icon.png" alt="linkedin">
+                        </h3>
+                    </a>
+                    <p class="team-members__member--function">MEDIA VORMGEVER</p>
+                </div>
+                <div class="team-members__member">
+                    <img class="team-members__member--img" src="public/img/img_avatar.png" alt="person image">
+                    <a class="team-members__member--link" href="#">
+                        <h3 class="team-members__member--name">Jelle
+                            <img class="team-members__member--linkedin" src="/public/img/linked_in_icon.png" alt="linkedin">
+                        </h3>
+                    </a>
+                    <p class="team-members__member--function">MEDIA DEVELOPER</p>
+                </div>
+                <div class="team-members__member">
+                    <img class="team-members__member--img" src="public/img/img_avatar.png" alt="person image">
+                    <a class="team-members__member--link" href="#">
+                        <h3 class="team-members__member--name">Jochem
+                            <img class="team-members__member--linkedin" src="/public/img/linked_in_icon.png" alt="linkedin">
+                        </h3>
+                    </a>
+                    <p class="team-members__member--function">MEDIA DEVELOPER</p>
+                </div>
+                <div class="team-members__member">
+                    <img class="team-members__member--img" src="public/img/img_avatar.png" alt="person image">
+                    <a class="team-members__member--link" href="#">
+                        <h3 class="team-members__member--name">Joshua
+                            <img class="team-members__member--linkedin" src="/public/img/linked_in_icon.png" alt="linkedin">
+                        </h3>
+                    </a>
+                    <p class="team-members__member--function">MEDIA DEVELOPER</p>
+                </div>
+                <div class="team-members__member">
+                    <img class="team-members__member--img" src="public/img/img_avatar.png" alt="person image">
+                    <a class="team-members__member--link" href="#">
+                        <h3 class="team-members__member--name">Marco
+                            <img class="team-members__member--linkedin" src="/public/img/linked_in_icon.png" alt="linkedin">
+                        </h3>
+                    </a>
+                    <p class="team-members__member--function">MEDIA DEVELOPER</p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer">
+        <div class="layer">
+            <div class="footer__grid">
+                <div class="footer__grid__left">
+                    <div class="footer__grid__left__marketing">
+                        <h2 class="footer--h2">Online Marketing</h2>
+                        <a class="footer--a" href="#">SEA: Zoekmachine adverteren</a><br>
+                        <a class="footer--a" href="#">SEO: zoekmachine optimalisatie</a><br>
+                        <a class="footer--a" href="#">Google Shopping adverteren</a>
+                    </div>
+                    <div class="footer__grid__left__wordpress">
+                        <h2 class="footer--h2">Wordpress Development</h2>
+                        <a class="footer--a" href="#">WordPress Development</a><br>
+                        <a class="footer--a" href="#">WordPress website laten maken</a><br>
+                        <a class="footer--a" href="#">Webshop laten maken</a><br>
+                        <a class="footer--a" href="#">Webdesign</a>
+                    </div>
+                </div>
+                <div class="footer__grid__middle">
+                    <div class="footer__grid__middle__socialmedia">
+                        <h2 class="footer--h2">Social Media</h2>
+                        <p class="footer--p">Het bouwen van WordPress-websites en het goed vindbaar maken van websites door middel van SEA en
+                            SEO is onze passie. Lees hier onze visie op Online Marketing en WordPress Development.
+                        </p>
+                    </div>
+                    <div class="footer__grid__middle__media-icons">
+
+                    </div>
+                </div>
+                <div class="footer__grid__right">
+                    <div class="footer__grid__right__youngvisual">
+                        <h2 class="footer--h2">Young Visual</h2>
+                        <p class="footer--p">Contactweg 36<br>
+                            1014 AN Amsterdam<br>
+                            020 -12 34 567<br>
+                            info@youngvisual.nl
+                        </p>
+                    </div>
+                    <div class="footer__grid__right__registers">
+                        <p class="footer--p">KVK nummer: 12345678 <br>
+                            BTW nummer: NL123456789A12 <br>
+                            IBAN: NL12 INGB 0123 4567 89
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <hr class="footer--hr">
+
+            <div class="footer__bottomtext">
+                <div class="footer__bottomtext__top">
+                    <a class="footer--a footer__bottomtext__top__inline" href="#">Algemene voorwaarden</a>
+                    <p class="footer--a footer__bottomtext__top__inline"> | </p>
+                    <a class="footer--a footer__bottomtext__top__inline" href="#">Privacy Policy</a>
+                </div>
+                <p class="footer__bottomtext--copyright">©2019 Young Visual</p>
+            </div>
+        </div>
+    </footer>
+</body>
